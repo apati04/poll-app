f62316a088b7db76ecf932fc33108bcf6ffbfacf
Andrew Patipaksiri <apax714@gmail.com>
2017-04-01 12:39:19 -0700

    express server runnning

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ var express = require('express');
 var app = express();
 //middleware
 app.use(express.static('./public'));
+app.use(express.static('./node_modules/bootstrap/dist'))
+
+app.listen(3000);
+console.log('Listening on 3000');
